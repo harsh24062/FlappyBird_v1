1fda35487685fa75a9cf7eae97d783185d5314f3
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-04-22 11:41:08 +0530

    Added Game loop and started it.

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,12 +1,15 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import javax.swing.plaf.DimensionUIResource;
 
-public class FlappyBird extends JPanel {
+public class FlappyBird extends JPanel implements ActionListener {
 
     int screenWidth = 360;
     int screenHeight = 640;
@@ -26,6 +29,9 @@ public class FlappyBird extends JPanel {
     // game logic
     Bird bird;
 
+    // game loop
+    Timer gameLoop;
+
     FlappyBird(){
         setPreferredSize(new DimensionUIResource(screenWidth, screenHeight));
         setBackground(Color.BLUE);
@@ -37,18 +43,29 @@ public class FlappyBird extends JPanel {
         bottomPipeImage = new ImageIcon(getClass().getClassLoader().getResource("resources/images/bottompipe.png")).getImage();
 
         bird = new Bird(birdX, birdY, birdWidth, birdHeight, birdImage);
+
+        //game timer
+        gameLoop = new Timer(1000/60, this);
+        //start the timer
+        gameLoop.start();
     }
 
   
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
+        
         //Adding background image
         g.drawImage(backgroundImage, 0, 0, screenWidth, screenHeight, null);
 
         //Adding bird image
         g.drawImage(bird.birdImage, bird.birdX, bird.birdY, bird.birdWidth, bird.birdHeight, null);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint(); // it calls paintComponent(Graphics g) to draw new image 
     }
   
 
